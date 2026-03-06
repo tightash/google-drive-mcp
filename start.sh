42617ccd5853a96872462691baf27c6ccda55647
tightash <ashley@digitalashagency.com>
2026-03-06 12:54:25 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 mkdir -p /app/config
 echo "$GCP_OAUTH_KEYS_JSON" > /app/config/gcp-oauth.keys.json
 echo "$GOOGLE_TOKENS_JSON" > /app/config/tokens.json
-GOOGLE_DRIVE_OAUTH_CREDENTIALS=/app/config/gcp-oauth.keys.json
-GOOGLE_DRIVE_MCP_TOKEN_PATH=/app/config/tokens.json
+export GOOGLE_DRIVE_OAUTH_CREDENTIALS=/app/config/gcp-oauth.keys.json
+export GOOGLE_DRIVE_MCP_TOKEN_PATH=/app/config/tokens.json
 node dist/index.js
